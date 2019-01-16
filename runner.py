586da710d516bf022a139de02a9bde039022f628
BenMaydan <bmaydan2022@kehillahstudent.org>
2019-01-16 12:34:41 -0800

    Fix for making pickle file instead of not having an ending

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import cookie
 import save
 
 
-#save.file('cookiesData', 'wb', {'currentUser':0})
+#save.file('cookiesData.pickle', 'wb', {'currentUser':0})
 #save.file('doubleUpgrade.pickle', 'wb', {'cookiesPerClick':1, 'cost':500})
 #save.file('goldenCookie.pickle', 'wb', {'goldenCookieChance':1, 'cost':1000})
 #doubleUpgradeDict = save.file('doubleUpgrade.pickle', 'rb')
